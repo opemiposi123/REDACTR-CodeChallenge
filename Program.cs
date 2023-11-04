9f8c1318e436a3e02d6d9ed7eae038064ff10ce6
AbdulWaheed MahmoodAhmad <abdulwaheedmahmoodahmad@gmail.com>
2023-11-04 15:27:25 +0100

    some changes

--- Program.cs
+++ Program.cs
@@ -7,29 +7,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter the original text:");
-        string originalText = Console.ReadLine();
+string originalText = Console.ReadLine();
 
-        Console.WriteLine("Enter the words to be scrambled (separated by spaces):");
-        string wordsToScrambleInput = Console.ReadLine();
-        string[] wordsToScramble = wordsToScrambleInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine("Enter the words to be scrambled (separated by spaces):");
+string wordsToScrambleInput = Console.ReadLine();
+string[] wordsToScramble = wordsToScrambleInput.Split(' ');
 
-        Console.WriteLine("Enter the replacement words (separated by spaces):");
-        string replacementWordsInput = Console.ReadLine();
-        string[] replacementWords = replacementWordsInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine("Enter the replacement words (separated by spaces):");
+string replacementWordsInput = Console.ReadLine();
+string[] replacementWords = replacementWordsInput.Split(' ');
 
-        if (wordsToScramble.Length != replacementWords.Length)
-        {
-            Console.WriteLine("The number of words to scramble and replacement words must match.");
-            return;
-        }
+if (wordsToScramble.Length != replacementWords.Length)
+{
+    Console.WriteLine("The number of words to scramble and replacement words must match.");
+}
+else
+{
+    for (int i = 0; i < wordsToScramble.Length; i++)
+    {
+        originalText = originalText.Replace(wordsToScramble[i], replacementWords[i]);
+    }
 
-        string scrambledText = originalText;
-        for (int i = 0; i < wordsToScramble.Length; i++)
-        {
-            scrambledText = scrambledText.Replace(wordsToScramble[i], replacementWords[i]);
-        }
-
-        Console.WriteLine("Scrambled Text:");
-        Console.WriteLine(scrambledText);
+    Console.WriteLine("Scrambled Text:");
+    Console.WriteLine(originalText);
+}
     }
 }
